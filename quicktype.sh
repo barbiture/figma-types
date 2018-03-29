2f17e58250a19190b747d6e725ca42e065699d84
David Siegel <dasiege@microsoft.com>
2018-03-28 20:19:53 -0700

    Try types at top-level
    This is better for GitHub browsing

--- quicktype.sh
+++ quicktype.sh
@@ -5,14 +5,14 @@ set -xe
 SCHEMA=figma-schema.json
 QUICKTYPE="quicktype $SCHEMA -s schema"
 
-$QUICKTYPE -o src/csharp/Figma.cs
-$QUICKTYPE -o src/go/figma.go
-$QUICKTYPE -o src/rust/figma.rs
-$QUICKTYPE -o src/cpp/figma.cpp
-$QUICKTYPE -o src/objective-c/FGFigma.m
-$QUICKTYPE -o src/java/Figma.java
-$QUICKTYPE -o src/typescript/figma.ts
-$QUICKTYPE -o src/javascript/figma.js
-$QUICKTYPE -o src/flow/figma.flow
-$QUICKTYPE -o src/swift/Figma.swift
-$QUICKTYPE -o src/elm/figma.elm
+$QUICKTYPE -o csharp/Figma.cs
+$QUICKTYPE -o go/figma.go
+$QUICKTYPE -o rust/figma.rs
+$QUICKTYPE -o cpp/figma.cpp
+$QUICKTYPE -o objective-c/FGFigma.m
+$QUICKTYPE -o java/Figma.java
+$QUICKTYPE -o typescript/figma.ts
+$QUICKTYPE -o javascript/figma.js
+$QUICKTYPE -o flow/figma.flow
+$QUICKTYPE -o swift/Figma.swift
+$QUICKTYPE -o elm/figma.elm
